0f9763fd36b898c2f133cc69e90a837a91d2b089
EngHack7 <hamzaz.k.hasan@st.ahu.edu.jo>
2020-06-16 21:25:08 +0300

    add merque footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import {BrowserRouter as Router ,Route} from 'react-router-dom';
 import Join from './component/Join/Join'
 import Chat from './component/Chat/Chat'
+import Footer from "./component/footer/Footer"
 
 const  App = () => 
     (
+        
         <Router>
             <Route path="/" exact component={Join} />
             <Route path="/chat" component={Chat} />
-
+            <Footer/>
         </Router>
+        
+        
     );
 
 
---
+++ client/src/component/footer/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./footer.css";
+function Footer() {
+
+    return ( 
+    <footer style={{backgroundColor: "#1A1A1D"}}>
+    
+    
+
+        
+      <marquee><h3 style={{color : "cornsilk", fontSize: "30px"}}> this site build by hamza kamel </h3></marquee>
+           
+      
+  
+      </footer>
+      );
+      
+  }
+  
+  export default Footer;
